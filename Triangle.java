70248d620a1193f7ca94641ca220f2f1ee6d9a18
ejFarr <roboproductions200@gmail.com>
2020-01-15 09:00:07 -0700

    change triangle

--- Triangle.java
+++ Triangle.java
@@ -1,14 +1,16 @@
 public class Triangle {
-    public static void main (String[] args) {
-        Point point1 = new Point();
-        Point point2 = new Point();
-        Point point3 = new Point();
+    private Point point1;
+    private Point point2;
+    private Point point3;
+    public Triangle(Point point1, Point point2, Point point3) {
+        this.point1 = point1;
+        this.point2 = point2;
+        this.point3 = point3;
 
         int base = point1.calcDistance(point1, point2);
         int slantHeight = point2.calcDistance(point2, point3);
         int height = (slantHeight * slantHeight) - (base * base);
         
         int area = (base * height)/2;
-        System.out.println("Area: " + area);
     }
 }
