5f2011b93bc8b0a3a222b8c97dc943db4b82b3f4
Caleb Matthew Schroder <schrodercm@s.dcsdk12.org>
2020-01-17 08:22:00 -0700

    done?

---
+++ Overlap.java
@@ -0,0 +1,9 @@
+public class Overlap {
+    private Square square1;
+    private Square square2;
+
+    public Overlap(Square square1, Square square2) {
+        this.square1 = square1;
+        this.square2 = square2;
+    }
+}
--- Square.java
+++ Square.java
@@ -2,11 +2,13 @@ public class Square {
 
     private Point p1;
     private Point p2;
+  
  
 
     public Square (Point p1, Point p2) {
         this.p1 = p1;
         this.p2 = p2;
+        
     }
 
     public double perimeter(Point p1, Point p2) {
@@ -19,6 +21,28 @@ public class Square {
         double distance = Point.calcDistance(p1, p2);
         area =  distance * distance;
         return area;
+    }
+
+    public static boolean isOverlap(Square sq1, Square sq2) {
+        yBound1 = sq1.p1.getY();
+        yBound2 = sq1.p2.getY();
+        xBound1 = sq1.p1.getX();
+        xBound2 = sq1.p2.getX();
+
+        y2Bound1 = sq2.p1.getY();
+        y2Bound2 = sq2.p2.getY();
+        x2Bound1 = sq2.p1.getX();
+        x2Bound2 = sq2.p2.getX();
+
+        if (y2Bound1 > yBound1 && y2Bound1 < yBound2 && x2Bound1 > xBound1 && x2Bound1 < xBound2) {
+            return true;
+        } 
+        if (y2Bound2 > yBound1 && y2Bound2 < yBound2 && x2Bound2 > xBound1 && x2Bound2 < xBound2 ) {
+            return true;
+        }
+        return false;
+
+
     }
 
     
