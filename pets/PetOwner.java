8ef3d8d1d980e2b1f3653b3f0a0913e1c30a5db1
Caleb Matthew Schroder <schrodercm@s.dcsdk12.org>
2020-01-22 08:24:17 -0700

    pet owner wip

---
+++ pets/PetOwner.java
@@ -0,0 +1,16 @@
+public class PetOwner {
+    private Dog bob;
+    private Cat marlee;
+
+    public PetOwner (Dog bob, Cat marlee) {
+        this.Dog = Dog;
+        this.Dog = Dog;
+    }
+
+    public boolean isHappy() {
+        if (bob.isHappy().equals("WOOF!") && marlee.isHappy().equals("meow")) {
+            return true;
+        }
+        return false;
+    }
+}
