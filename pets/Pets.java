f9a210839a03250503fb9b7ca1bb21a739ffa057
Caleb Matthew Schroder <schrodercm@s.dcsdk12.org>
2020-01-22 09:17:26 -0700

    yes

---
+++ pets/Pets.java
@@ -0,0 +1,13 @@
+public class Pet {
+    private String name;
+
+    private boolean hungry;
+    private boolean walked;
+    private boolean petted;
+
+    public Pets(String name) {
+        this.name = name;
+    }
+    
+
+}
